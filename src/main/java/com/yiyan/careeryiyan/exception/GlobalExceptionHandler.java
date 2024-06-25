8247983147b21f16cda7d0bcec0e07f6df6dec9f
pcpas <connorzhang997@outlook.com>
2024-06-25 12:13:08 +0800

    feature: exception handler

---
+++ src/main/java/com/yiyan/careeryiyan/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.yiyan.careeryiyan.exception;
+
+import com.yiyan.careeryiyan.model.response.StringResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器，处理项目中抛出的业务异常
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    /**
+     * 捕获业务异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public ResponseEntity<StringResponse> exceptionHandler(BaseException ex){
+        log.error("异常信息：{}", ex.getMessage());
+        return ResponseEntity.internalServerError().body(new StringResponse(ex.getMessage()));
+    }
+
+}
