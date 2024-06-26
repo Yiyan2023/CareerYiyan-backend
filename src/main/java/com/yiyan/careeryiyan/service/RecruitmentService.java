4de6107c256dd2e0573a3bb05303c43ff371d19c
yunlong <yunlonghere@gmail.com>
2024-06-26 20:21:52 +0800

    fix: update offerCount when deliver offer and defer offer

--- src/main/java/com/yiyan/careeryiyan/service/RecruitmentService.java
+++ src/main/java/com/yiyan/careeryiyan/service/RecruitmentService.java
@@ -1,5 +1,6 @@
 package com.yiyan.careeryiyan.service;
 
+import com.yiyan.careeryiyan.exception.BaseException;
 import com.yiyan.careeryiyan.mapper.ApplyMapper;
 import com.yiyan.careeryiyan.mapper.RecruitmentMapper;
 import com.yiyan.careeryiyan.model.domain.Apply;
@@ -10,6 +11,7 @@ import com.yiyan.careeryiyan.model.request.EditRecruitmentRequest;
 import com.yiyan.careeryiyan.model.response.UserApplyDetailResponse;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,11 +21,12 @@ public class RecruitmentService {
     RecruitmentMapper recruitmentMapper;
     @Resource
     ApplyMapper applyMapper;
-    public int addRecruitment(AddRecruitmentRequest addRecruitmentRequest){
+
+    public int addRecruitment(AddRecruitmentRequest addRecruitmentRequest) {
         return recruitmentMapper.addRecruitment(addRecruitmentRequest);
     }
 
-    public List<Recruitment> getRecruitmentList(String enterpriseId){
+    public List<Recruitment> getRecruitmentList(String enterpriseId) {
         return recruitmentMapper.getRecruitmentList(enterpriseId);
     }
 
@@ -56,8 +59,33 @@ public class RecruitmentService {
         return recruitmentMapper.getUserApplyList(id);
     }
 
+    @Transactional(rollbackFor = Exception.class)
     public int changeState(String applyId, int status) {
-        return applyMapper.changeState(applyId, status);
+        if (status == 1 || status == 4) {
+            //发offer
+            try {
+                // 获取加锁的Apply记录
+                applyMapper.getApplyAndRecruitmentForUpdate(applyId);
+                int rowsAffected = 0;
+                // 执行更新操作
+                if (status == 1) {
+                    rowsAffected = applyMapper.increaseOfferCountAndChangeStatus(applyId, status);
+                } else {
+                    rowsAffected = applyMapper.decreaseOfferCountAndChangeStatus(applyId, status);
+                }
+                // 检查更新是否成功
+                if (rowsAffected != 2) {
+                    System.out.println("rowsAffected="+rowsAffected);
+                    //事务回滚
+                    throw new IllegalStateException("Failed to update apply status");
+                }
+            } catch (Exception e) {
+                // 记录异常日志
+                System.out.println(e);
+                throw e;
+            }
+        }
+        return applyMapper.changeStatus(applyId, status);
     }
 
     public Apply getApplyById(String applyId) {
