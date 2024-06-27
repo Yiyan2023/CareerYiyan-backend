94b2bbe72ced4f52c58c346d4b0abc0a65f76ae1
cheapcheer <20374151@buaa.edu.cn>
2024-06-27 09:33:44 +0800

    fix:getApplicationList增加返回信息

---
+++ src/main/java/com/yiyan/careeryiyan/model/domain/UserJobPreferences.java
@@ -0,0 +1,10 @@
+package com.yiyan.careeryiyan.model.domain;
+
+import lombok.Data;
+
+@Data
+public class UserJobPreferences {
+    private String id;
+    private String userId;
+    private String recruitmentTag;
+}
