ab107cc2e47760faa4db90fcfc8205b71a1d3710
yunlong <yunlonghere@gmail.com>
2024-06-30 10:07:37 +0800

    fix: filed name in search

--- src/main/java/com/yiyan/careeryiyan/mapper/SearchMapper.java
+++ src/main/java/com/yiyan/careeryiyan/mapper/SearchMapper.java
@@ -17,45 +17,45 @@ public interface SearchMapper {
 
 
     @Select("select *,r.ep_id as ep_id ,\n" +
-            "       MATCH(r.rc_name) AGAINST(#{recruitmentName} IN NATURAL LANGUAGE MODE) as score\n" +
+            "       MATCH(r.rc_name) AGAINST(#{rcName} IN NATURAL LANGUAGE MODE) as score\n" +
             "from recruitment r join enterprise e on r.ep_id = e.ep_id\n" +
-            "         where e.is_delete=0 and r.is_delete=0 and MATCH(r.rc_name) AGAINST(#{recruitmentName} IN NATURAL LANGUAGE MODE)\n" +
+            "         where e.is_delete=0 and r.is_delete=0 and MATCH(r.rc_name) AGAINST(#{rcName} IN NATURAL LANGUAGE MODE)\n" +
             "order by score desc\n" +
-            "limit #{pageSize},#{pageNum};"
+            "limit #{offset},#{pageSize};"
     )
     List<Map<String,Object>> searchRecruitment(SearchRecruitmentRequest searchRecruitmentRequest);
 
     @Select("select count(*)\n" +
             "from recruitment r join enterprise e on r.ep_id = e.ep_id\n" +
-            "         where e.is_delete=0 and r.is_delete=0 and MATCH(r.rc_name) AGAINST(#{recruitmentname} IN NATURAL LANGUAGE MODE);")
+            "         where e.is_delete=0 and r.is_delete=0 and MATCH(r.rc_name) AGAINST(#{rcName} IN NATURAL LANGUAGE MODE);")
     int getSearchRecruitmentTotal(SearchRecruitmentRequest searchRecruitmentRequest);
 
     //单表查询，不需要手动映射名字
-    @Select("select * ,MATCH(ep_name) AGAINST(#{enterpriseName} IN NATURAL LANGUAGE MODE) as score\n" +
+    @Select("select * ,MATCH(ep_name) AGAINST(#{epName} IN NATURAL LANGUAGE MODE) as score\n" +
             "from enterprise\n" +
-            "where MATCH(ep_name) AGAINST(#{enterpriseName} IN NATURAL LANGUAGE MODE) and is_delete=0\n" +
+            "where MATCH(ep_name) AGAINST(#{epName} IN NATURAL LANGUAGE MODE) and is_delete=0\n" +
             "order by score desc\n" +
-            "limit #{pageSize},#{pageNum};")
+            "limit #{offset},#{pageSize};")
     List<Map<String,Object>>  searchEnterprise(SearchEnterpriseRequest searchEnterpriseRequest);
 
     @Select("select count(*)\n" +
             "from enterprise\n" +
-            "where MATCH(ep_name) AGAINST(#{enterpriseName} IN NATURAL LANGUAGE MODE) and  is_delete=0;")
+            "where MATCH(ep_name) AGAINST(#{epName} IN NATURAL LANGUAGE MODE) and  is_delete=0;")
     int getSearchEnterpriseTotal(SearchEnterpriseRequest searchEnterpriseRequest);
 
     @Select("select *,u.user_id as user_id,e.ep_id=eu.ep_id as ep_id," +
-            "MATCH(user_name) AGAINST(#{userName} IN NATURAL LANGUAGE MODE) as score\n" +
+            "MATCH(user_nickname) AGAINST(#{userNickname} IN NATURAL LANGUAGE MODE) as score\n" +
             "from user u join enterprise_user eu on u.user_id = eu.user_id join enterprise e on eu.ep_id = e.ep_id\n" +
-            "where MATCH(user_name) AGAINST(#{userName} IN NATURAL LANGUAGE MODE) " +
+            "where MATCH(user_nickname) AGAINST(#{userNickname} IN NATURAL LANGUAGE MODE) " +
             "and u.is_delete=0 and e.is_delete=0 and eu.is_delete=0\n" +
             "ORDER BY score DESC\n" +
-            "LIMIT #{offset}, #{pageSize};")
+            "limit #{offset},#{pageSize};")
     List<Map<String,Object>>  searchUser(SearchUserRequest searchUserRequest);
 
     @Select("select count(*)\n" +
             "from user u join enterprise_user eu on u.user_id = eu.user_id join enterprise e on eu.ep_id = e.ep_id\n" +
             "where u.is_delete=0 and e.is_delete=0 and eu.is_delete=0\n" +
-            "and MATCH(user_name) AGAINST(#{userName} IN NATURAL LANGUAGE MODE);")
+            "and MATCH(user_nickname) AGAINST(#{userNickname} IN NATURAL LANGUAGE MODE);")
     int getSearchUserTotal(SearchUserRequest searchUserRequest);
 
 
